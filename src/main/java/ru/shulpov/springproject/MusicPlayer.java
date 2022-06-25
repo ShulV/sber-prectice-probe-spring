875aba2020dedb2fcd3d77dec338e8a918898ff9
Victor Desctop <vshulpov@gmail.com>
2022-06-25 16:35:12 +0700

    Dependency Injection via setter with ref (lesson 6)

--- src/main/java/ru/shulpov/springproject/MusicPlayer.java
+++ src/main/java/ru/shulpov/springproject/MusicPlayer.java
@@ -3,9 +3,17 @@ package ru.shulpov.springproject;
 public class MusicPlayer {
     private Music music;
 
+    public void setMusic(Music music) {
+        this.music = music;
+    }
+
     // IoC
     public MusicPlayer(Music music) {
         this.music = music;
+    }
+
+    public MusicPlayer() {
+
     }
 
     public void playMusic() {
