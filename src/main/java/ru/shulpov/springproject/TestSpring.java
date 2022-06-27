1d78a38fd5cffe2abb630c2bae974b8d2fa839c8
Victor Desctop <vshulpov@gmail.com>
2022-06-28 01:39:43 +0700

    config by java-code (lesson 13.1)

--- src/main/java/ru/shulpov/springproject/TestSpring.java
+++ src/main/java/ru/shulpov/springproject/TestSpring.java
@@ -1,15 +1,13 @@
 package ru.shulpov.springproject;
 
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import java.io.IOException;
 
 public class TestSpring {
     public static void main(String[] args) throws IOException {
-        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(
-          "applicationContext.xml"
-        );
-
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(SpringConfig.class);
         MusicPlayer musicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
 
         musicPlayer.playMusic(MusicGenre.RAP);
