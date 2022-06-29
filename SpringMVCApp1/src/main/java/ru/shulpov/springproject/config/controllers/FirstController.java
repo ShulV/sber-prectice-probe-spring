2e5ca24ea670a25fcab77e64a2426c66ef42d793
Victor Desctop <vshulpov@gmail.com>
2022-06-29 16:33:30 +0700

    get req params (@RequestParam) (lesson 19.1)

--- SpringMVCApp1/src/main/java/ru/shulpov/springproject/config/controllers/FirstController.java
+++ SpringMVCApp1/src/main/java/ru/shulpov/springproject/config/controllers/FirstController.java
@@ -3,6 +3,7 @@ package ru.shulpov.springproject.config.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.servlet.http.HttpServletRequest;
 
@@ -10,9 +11,8 @@ import javax.servlet.http.HttpServletRequest;
 @RequestMapping("/first")
 public class FirstController {
     @GetMapping("/hello")
-    public String sayHello(HttpServletRequest req) {
-        String name = req.getParameter("name");
-        String age = req.getParameter("age");
+    public String sayHello(@RequestParam(value = "name", required = false) String name,
+                           @RequestParam(value = "age", required = false) String age) {
         System.out.println("Привет, " + name + ", твой возраст - " + age);
         return "first/hello";
     }
